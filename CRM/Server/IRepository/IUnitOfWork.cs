e68e9671ba846a89e07c08990d388afcb2aa4d6a
LEE SEE MENG 2201267H <2201267H@student.tp.edu.sg>
2024-02-10 17:51:53 +0800

    Added REST API Endpoints and Unit Of Work pattern.

---
+++ CRM/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using CRM.Server.IRepository;
+using CRM.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task Save(HttpContext httpContext);
+        IGenericRepository<Make> Makes { get; }
+        IGenericRepository<Model> Models { get; }
+        IGenericRepository<Vehicle> Vehicles { get; }
+        IGenericRepository<Colour> Colours { get; }
+        IGenericRepository<Booking> Bookings { get; }
+        IGenericRepository<Customer> Customers { get; }
+    }
+}
